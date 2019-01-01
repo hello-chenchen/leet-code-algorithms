306d693a6b43923e16d402fd05050f0ad9df6e2d
hello_chenchen <welcomechenchen@gmail.com>
2019-01-02 02:07:45 +0800

    two-sum 0.0.1

---
+++ two-sum/solution.cc
@@ -0,0 +1,11 @@
+//LINK:https://leetcode.com/problems/two-sum/
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
