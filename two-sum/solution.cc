f82163ed1a62daed6e8c9b76e28409868c961638
hello-chenchen <welcomechenchen@gmail.com>
2019-01-07 00:44:44 +0800

    two-sum 0.0.2

--- two-sum/solution.cc
+++ two-sum/solution.cc
@@ -1,11 +1,44 @@
 //LINK:https://leetcode.com/problems/two-sum/
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    static vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ret;
+        if(nums.size() < 2) {
+            cout << "nums size: " << nums.size() << endl;
+            return ret;
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            for(int n = 1; n < nums.size(); n++) {
+                int num_i = nums[i];
+                int num_n = nums[n];
+                if(num_i + num_n == target) {
+                    ret.push_back(i);
+                    ret.push_back(n);
+                    return ret;
+                }
+            }
+        }
+
+        return ret;
     }
 };
+
+main(int argc, char const *argv[])
+{
+    vector<int> nums {2, 7, 11, 15};
+    vector<int> ret = Solution().twoSum(nums, 9);
+
+    if(2 == ret.size()) {
+        cout << "result: " << ret[0] << " and " << ret[1] << endl;
+    } else {
+        cout << "input error!";
+    }
+
+    return 0;
+}
