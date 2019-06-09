20ca24fbfae53a22523ccc219ceef1a2266bcc45
hello-chenchen <welcomechenchen@gmail.com>
2019-06-09 14:26:31 +0800

    fuction-pre: roman-to-integer

---
+++ roman-to-integer/solution.cc
@@ -0,0 +1,34 @@
+//LINK: https://leetcode-cn.com/problems/roman-to-integer/
+//COMPILE:g++ two-sum/solution.cc -o tmp/main -std=c++11
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum RomanWords {
+    EN_I = 1,
+    EN_IV = 4,
+    EN_V = 5,
+    EN_IX = 9,
+    EN_XL = 40,
+    EN_L = 50,
+    EN_XC = 90,
+    EN_C = 100,
+    EN_CD = 400,
+    EN_D = 500,
+    EN_CM = 900,
+    EN_M = 1000
+};
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
