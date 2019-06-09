2f601e2a3dc363c57cf75348c41e08ae4c2c06d2
hello-chenchen <welcomechenchen@gmail.com>
2019-06-09 11:50:31 +0800

    optimize: use map to search

--- two-sum/solution.cc
+++ two-sum/solution.cc
@@ -2,33 +2,27 @@
 //COMPILE:g++ two-sum/solution.cc -o tmp/main -std=c++11
 #include <vector>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     static vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ret;
-        if(nums.size() < 2) {
-            cout << "nums size: " << nums.size() << endl;
-            return ret;
-        }
-
+        map<int, int> numMap;
+        vector<int> result;
         for(int i = 0; i < nums.size(); i++) {
-            for(int n = i + 1; n < nums.size(); n++) {
-                if(n == i)
-                    continue;
-                int num_i = nums[i];
-                int num_n = nums[n];
-                if(num_i + num_n == target) {
-                    ret.push_back(i);
-                    ret.push_back(n);
-                    return ret;
-                }
+            int num2 = target - nums[i];
+            auto search = numMap.find(num2);
+            if(search != numMap.end()) {
+                result.push_back(search->second);
+                result.push_back(i);
+            } else {
+                numMap.insert(std::pair<int, int>(nums[i], i));
             }
         }
 
-        return ret;
+        return result;
     }
 };
 
