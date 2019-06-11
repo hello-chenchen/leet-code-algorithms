1197fd759e58193b96db5618b5da990edf8c065b
hello-chenchen <welcomechenchen@gmail.com>
2019-06-11 10:15:39 +0800

    bug: romantoint return 0

--- roman-to-integer/solution.cc
+++ roman-to-integer/solution.cc
@@ -2,33 +2,81 @@
 //COMPILE:g++ two-sum/solution.cc -o tmp/main -std=c++11
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
-
-enum RomanWords {
-    EN_I = 1,
-    EN_IV = 4,
-    EN_V = 5,
-    EN_IX = 9,
-    EN_XL = 40,
-    EN_L = 50,
-    EN_XC = 90,
-    EN_C = 100,
-    EN_CD = 400,
-    EN_D = 500,
-    EN_CM = 900,
-    EN_M = 1000
-};
 
 class Solution {
 public:
     int romanToInt(string s) {
-        
+        int result = 0;
+        while(s.length() >= 2) {
+            string firstChars = s.substr(0, 2);
+            string replaceChars = s.replace(0, 2, "");
+            int twoChars = convert2Word(firstChars);
+            if( 0 == twoChars ) {
+                string firstChar = firstChars.substr(0, 1);
+                twoChars = convert1Word(firstChar);
+                string secondChar = firstChars.substr(1, 1);
+                twoChars += convert1Word(secondChar);
+            }
+
+            result += twoChars;
+        }
+
+        if(result < 1 || result > 3999) {
+            return 0;
+        }
+
+        return result;
+    }
+
+private:
+    int convert2Word(string word) {
+        if(word.size() < 2) {
+            return 0;
+        }
+
+        map<string, int> Roman2Words({ {"IV", 4}, {"IX", 9},
+            {"XL", 40}, {"XC", 90}, {"CD", 400}, {"CM", 900} });
+
+        map<string, int>::iterator itr = Roman2Words.begin();
+        while(itr != Roman2Words.end()) {
+            if(0 == word.compare(itr->first)) {
+                return itr->second;
+            } else {
+                itr++;
+            }
+        }
+
+        return 0;
+    }
+
+    int convert1Word(string word) {
+        if(word.size() < 1) {
+            return 0;
+        }
+
+        map<string, int> Roman1Words({ {"I", 1}, {"V", 5},
+            {"L", 50}, {"C", 100}, {"D", 500}, {"M", 1000} });
+
+        map<string, int>::iterator itr = Roman1Words.begin();
+        while(itr != Roman1Words.end()) {
+            if(0 == word.compare(itr->first)) {
+                return itr->second;
+            } else {
+                itr++;
+            }
+        }
+
+        return 0;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-
+    Solution aa;
+    int bb = aa.romanToInt("I");
+    cout << "bb: " << bb << endl;
     return 0;
 }
