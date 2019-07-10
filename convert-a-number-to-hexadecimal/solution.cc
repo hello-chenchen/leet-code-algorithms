b71dadd5969a5e629a841ef2c4601554a4310834
hello_chenchen <welcomechenchen@gmail.com>
2019-07-11 00:45:54 +0800

    optimize: reduce code

--- convert-a-number-to-hexadecimal/solution.cc
+++ convert-a-number-to-hexadecimal/solution.cc
@@ -6,42 +6,20 @@ using namespace std;
 class Solution {
 public:
     string toHex(int num) {
-        string res = "";
-        if(0 == num) {
-            return "0";
+        if (num == 0) return "0";
+        string hex = "0123456789abcdef", ans = "";
+        while(num && ans.size() < 8){
+            ans = hex[num & 0xf] + ans;
+            num >>= 4;
         }
-
-        string result = "";
-        toHex(num, result);
-
-        for(int i =result.length() - 1; i >= 0; i--) {
-            res += result[i];
-        }
-
-        return res;
+        return ans;
     }
-
-private:
-    void toHex(unsigned int num, string& hexStr) {
-        if(0 == num) {
-            return;
-        }
-        int restLeft = num >> 4;
-        int rightNum = num & 0x0000000f;
-        auto result = m_charInt.find(rightNum);
-        hexStr.append(result->second);
-        toHex(restLeft, hexStr);
-    }
-
-    const map<int, string> m_charInt = {{0,"0"}, {1,"1"},{2,"2"},
-        {3,"3"}, {4,"4"}, {5,"5"}, {6,"6"},{7,"7"}, {8,"8"}, {9,"9"},
-        {10,"a"},{11,"b"}, {12,"c"},{13,"d"}, {14,"e"}, {15,"f"}};
 };
 
 int main(int argc, char const *argv[])
 {
     /* code */
     Solution cc;
-    cout << cc.toHex(-2147483648) << endl;
+    cout << cc.toHex(26) << endl;
     return 0;
 }
