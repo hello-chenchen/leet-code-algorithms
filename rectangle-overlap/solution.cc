10058e9b10a532e13fcd7d050c8df678a82b185d
hello_chenchen <welcomechenchen@gmail.com>
2019-07-11 10:49:14 +0800

    optimize: reduce code

--- rectangle-overlap/solution.cc
+++ rectangle-overlap/solution.cc
@@ -6,15 +6,7 @@ using namespace std;
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        int rec1X1 = rec1[0], rec1Y1 = rec1[1], rec1X2 = rec1[2], rec1Y2 = rec1[3];
-        int rec2X1 = rec2[0], rec2Y1 = rec2[1], rec2X2 = rec2[2], rec2Y2 = rec2[3];
-        if(rec2X1 < rec1X1 && rec2Y1 < rec1Y2 && rec2X2 > rec1X1 && rec2Y2 > rec1Y1) {
-            return true;
-        } else if(rec2X1 < rec1X2 && rec2Y2 > rec1Y1 && rec2X2 > rec1X1 && rec2Y1 < rec1Y2) {
-            return true;
-        }
-
-        return false;
+        return !(rec1[0] >= rec2[2] || rec1[2] <= rec2[0] || rec1[1] >= rec2[3] || rec1[3] <= rec2[1]);
     }
 };
 
