4475efe4f63b892c80949aafd86a8922b1b3e37c
hello_chenchen <welcomechenchen@gmail.com>
2019-08-03 21:45:43 +0800

    feature: fix leetcode 26

--- leetcode-26/soluction.cc
+++ leetcode-26/soluction.cc
@@ -1,5 +1,10 @@
+// #include "./../cclib/inc/adt/vector.h"
 #include <vector>
+#include "./../cclib/cclib-common/inc/util/util.h"
 
+using namespace cclib;
+// using namespace cclib::adt;
+using namespace cclib::common::util;
 using namespace std;
 
 class Solution {
@@ -11,19 +16,27 @@ public:
 
         int temp = nums[0];
         vector<int>::iterator itr = nums.begin() + 1;
-        for(int i = 1; i < nums.size(); i++) {
-            // vector<int>::iterator tmp = itr;
-            if(temp == nums[i]) {
+        int size = nums.size();
+        while(itr != nums.end()) {
+            vector<int>::iterator tmp = itr;
+            if(temp == *itr) {
                 nums.erase(itr);
             } else {
+                temp = *itr;
                 itr++;
             }
         }
+        return nums.size();
     }
 };
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    vector<int> aa = {0,0,1,1,1,2,2,3,3,4};
+    Solution bb;
+    int cc = bb.removeDuplicates(aa);
+    cout << cc << endl;
+    printVectorValue(aa);
     return 0;
 }
