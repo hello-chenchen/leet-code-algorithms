1f96da45dcf31378b7048237b903ea3f9fe4bb5f
hello_chenchen <welcomechenchen@gmail.com>
2019-08-04 12:41:53 +0800

    optimize: optimize leetcode 26

--- leetcode-26/soluction.cc
+++ leetcode-26/soluction.cc
@@ -1,31 +1,30 @@
-// #include "./../cclib/inc/adt/vector.h"
-#include <vector>
+#include "./../cclib/inc/adt/vector.h"
+// #include <vector>
 #include "./../cclib/cclib-common/inc/util/util.h"
 
 using namespace cclib;
-// using namespace cclib::adt;
+using namespace cclib::adt;
 using namespace cclib::common::util;
-using namespace std;
+// using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if(nums.size() <= 1) {
-            return nums.size();
-        }
-
-        int temp = nums[0];
-        vector<int>::iterator itr = nums.begin() + 1;
+    int removeDuplicates(Vector<int>& nums) {
+        int loop = 1, diff = 0;
         int size = nums.size();
-        while(itr != nums.end()) {
-            vector<int>::iterator tmp = itr;
-            if(temp == *itr) {
-                nums.erase(itr);
-            } else {
-                temp = *itr;
-                itr++;
+
+        //O(n)
+        for(; loop < size; loop++) {
+            if(nums[loop] != nums[diff]) {
+                nums[++diff] = nums[loop];
             }
         }
+
+        //O(n)
+        for(int i = nums.size() - 1; i > diff; i--) {
+            nums.pop_back();
+        }
+
         return nums.size();
     }
 };
@@ -33,10 +32,9 @@ public:
 int main(int argc, char const *argv[])
 {
     /* code */
-    vector<int> aa = {0,0,1,1,1,2,2,3,3,4};
+    Vector<int> aa = {0,0,1,1,1,2,2,3,3,4};
     Solution bb;
     int cc = bb.removeDuplicates(aa);
-    cout << cc << endl;
     printVectorValue(aa);
     return 0;
 }
