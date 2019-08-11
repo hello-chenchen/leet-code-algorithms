697b91437a2544b159da6415d74fbefc611735dc
hello_chenchen <welcomechenchen@gmail.com>
2019-08-11 23:13:50 +0800

    feature: support deque use list for leetcode 225

---
+++ leetcode-225/soluction.cc
@@ -0,0 +1,41 @@
+#include <list>
+
+using namespace std;
+
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+        _M_val = new list<int>();
+    }
+
+    ~MyStack() {
+        delete _M_val;
+        _M_val = NULL;
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        _M_val->push_back(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int result = _M_val->back();
+        _M_val->pop_back();
+        return result;
+    }
+
+    /** Get the top element. */
+    int top() {
+        return _M_val->back();
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return 0 == _M_val->size();
+    }
+
+private:
+    list<int>* _M_val;
+};
