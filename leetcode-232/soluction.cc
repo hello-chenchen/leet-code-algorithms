7326b8c9a799a3b839653f00d6ec24367eb2dd40
hello_chenchen <welcomechenchen@gmail.com>
2019-08-21 17:51:54 +0800

    feature: support leetcode-232

--- leetcode-232/soluction.cc
+++ leetcode-232/soluction.cc
@@ -6,36 +6,55 @@ class MyQueue {
 public:
     /** Initialize your data structure here. */
     MyQueue() {
-        _M_val = new stack<int>();
+        _M_push = new stack<int>();
+        _M_pop = new stack<int>();
     }
 
     ~MyQueue() {
-        delete _M_val;
-        _M_val = NULL;
+        delete _M_push;
+        _M_push = NULL;
+
+        delete _M_pop;
+        _M_pop = NULL;
     }
 
     /** Push element x to the back of queue. */
     void push(int x) {
-        _M_val->push(x);
-        int start = 0;
-        int end = _M_val->size();
+        _M_push->push(x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        int result = _M_val->
+        if(_M_pop->empty()) {
+            while(!_M_push->empty()) {
+                _M_pop->push(_M_push->top());
+                _M_push->pop();
+            }
+        }
+
+        int result = _M_pop->top();
+        _M_pop->pop();
+        return result;
     }
 
     /** Get the front element. */
     int peek() {
+        if(_M_pop->empty()) {
+            while(!_M_push->empty()) {
+                _M_pop->push(_M_push->top());
+                _M_push->pop();
+            }
+        }
 
+        return _M_pop->top();
     }
 
     /** Returns whether the queue is empty. */
     bool empty() {
-
+        return _M_push->empty() && _M_pop->empty();
     }
 
 private:
-    stack<int>* _M_val;
+    stack<int>* _M_push;
+    stack<int>* _M_pop;
 };
