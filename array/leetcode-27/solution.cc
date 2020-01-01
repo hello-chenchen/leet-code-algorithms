352577ea4fb4b6d6bd4ac33d3c8831f95a3a3a4f
hello_chenchen <welcomechenchen@gmail.com>
2020-01-01 23:42:26 +0800

    array-27: still error

--- array/leetcode-27/solution.cc
+++ array/leetcode-27/solution.cc
@@ -1,26 +1,36 @@
 #include <vector>
+#include <iostream>
+#include "../../cclib/cclib-common/inc/util/util.h"
 
 using namespace std;
+using namespace cclib;
 
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        if(nums.size() == 1) {
-            if()
-        }
         int temp = 0;
-        int tempSize = 0;
-        for(int i = 0; i + tempSize < nums.size() - 1; i++) {
+        int vectorSize = 0;
+        for(int i = 0; i + vectorSize < nums.size() - 1; i++) {
             if(nums[i] == val) {
-                temp = nums[i];
-                nums[i] = nums[i+tempSize+1];
-                nums[i+tempSize+1] = temp;
-                tempSize++;
-                i--;
-            } else {
-
+                if(nums[i+vectorSize+1] != val) {
+                    temp = nums[i+vectorSize+1];
+                    nums[i+vectorSize+1] = nums[i];
+                    nums[i] = temp;
+                } else {
+                    vectorSize++;
+                }
             }
         }
+        cclib::common::util::printVectorValue(nums);
+        for(int i = nums.size() - 1; i <= nums.size() - 1 && i > 0; i--) {
+            if(nums[i] == val) {
+                nums.pop_back();
+                continue;
+            }
+            break;
+        }
+
+        return nums.size();
     }
 };
 
@@ -28,6 +38,9 @@ int main(int argc, char const *argv[])
 {
     /* code */
     Solution demo;
-    demo.removeElement();
+    vector<int> aaa{0,1,2,2,3,0,4,2};
+    int res = demo.removeElement(aaa, 2);
+    cout << res << endl;
+    cclib::common::util::printVectorValue(aaa);
     return 0;
 }
