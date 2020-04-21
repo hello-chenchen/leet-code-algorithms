2199cbb493f20d55f96b2b3c5277c2b94c643980
hello_chenchen <welcomechenchen@gmail.com>
2020-04-21 18:04:49 +0800

    binary search

--- array/leetcode-35/solution.cc
+++ array/leetcode-35/solution.cc
@@ -5,15 +5,20 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int result = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] >= target) {
-                result = i;
-                break;
-            } else {
-                result = i + 1;
-            }
+        return binarySearch(nums, target, 0, nums.size());
+    }
+
+private:
+    int binarySearch(vector<int>& nums, int target, int startIndex, int endIndex) {
+        int midIndex = (endIndex - startIndex) / 2;
+        if(nums[midIndex] > target) {
+            // midIndex = (startIndex + midIndex) / 2;
+            return binarySearch(nums, target, startIndex, midIndex);
+        } else if(nums[midIndex] < target) {
+            // midIndex = (midIndex + endIndex) / 2;
+            return binarySearch(nums, target, midIndex, endIndex);
+        } else {    //nums[midIndex] >= target
+            return midIndex;
         }
-        return result;
     }
 };
