3047547e7e4247f87e7f804f4992403a16d860a7
hello_chenchen <welcomechenchen@gmail.com>
2020-04-22 18:26:36 +0800

    support binary search

--- array/leetcode-35/solution.cc
+++ array/leetcode-35/solution.cc
@@ -1,24 +1,43 @@
 #include <vector>
+#include <iostream>
+#include "../../cclib/cclib-common/inc/util/util.h"
 
 using namespace std;
 
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return binarySearch(nums, target, 0, nums.size());
+        return binarySearch(nums, target, 0, nums.size() - 1);
     }
 
 private:
     int binarySearch(vector<int>& nums, int target, int startIndex, int endIndex) {
-        int midIndex = (endIndex - startIndex) / 2;
+        int midIndex = (endIndex + startIndex) / 2;
+        if(startIndex == midIndex) {
+            if(target <= nums[startIndex]) {
+                return startIndex;
+            } else if(target > nums[endIndex]) {
+                return endIndex + 1;
+            } else {
+                return startIndex + 1;
+            }
+        }
+
         if(nums[midIndex] > target) {
-            // midIndex = (startIndex + midIndex) / 2;
             return binarySearch(nums, target, startIndex, midIndex);
         } else if(nums[midIndex] < target) {
-            // midIndex = (midIndex + endIndex) / 2;
             return binarySearch(nums, target, midIndex, endIndex);
-        } else {    //nums[midIndex] >= target
+        } else {
             return midIndex;
         }
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    vector<int> input{1};
+    Solution solution;
+    int result = solution.searchInsert(input, 1);
+    cout << "result: " << result << endl;
+    return 0;
+}
