85fbf0ac666ec459985423d87e536e6cd5478987
hello_chenchen <welcomechenchen@gmail.com>
2020-05-24 22:16:52 +0800

    use while instead of call back

--- array/leetcode-66/soluction.cc
+++ array/leetcode-66/soluction.cc
@@ -11,17 +11,18 @@ public:
 
 private:
     vector<int> callBackPlus(vector<int>& digits, int index) {
+        while (digits[index] == 9 && index != 0) {
+            digits[index] = 0;
+            index--;
+        }
+
         if(digits[index] < 9) {
             digits[index] = digits[index] + 1;
-        } else {
+        } else if(0 == index) {
             digits[index] = 0;
-            if(0 == index) {
-                std::vector<int>::iterator it;
-                it = digits.begin();
-                it = digits.insert (it , 1);
-            } else {
-                callBackPlus(digits, index - 1);
-            }
+            std::vector<int>::iterator it;
+            it = digits.begin();
+            it = digits.insert (it , 1);
         }
 
         return digits;
@@ -32,6 +33,6 @@ int main(int argc, char const *argv[])
 {
     Solution a;
     vector<int> b({0,11,2});
-    
+
     return 0;
 }
