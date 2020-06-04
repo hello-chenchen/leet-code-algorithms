2685e51ab05c055fc0dac5da24cbfad6c3b73ab2
hello_chenchen <welcomechenchen@gmail.com>
2020-06-04 23:22:54 +0800

    support leetcode-88

--- array/leetcode-88/solution.cc
+++ array/leetcode-88/solution.cc
@@ -7,34 +7,44 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = 0; i < n; i++) {
-            cout << "i: " << i << " : " << nums2[i] << endl;
-            for(int j = 0; j < m + n; j++) {
-                cout << "i: " << i << " : " << nums2[i] << " j: " << j << " : " << nums1[j] << endl;
-                if(nums2[i] < nums1[j]) {
-                    swap(nums2[i], nums1[j]);
-                    i--;
-                    break;
-                }
-
-                if(0 == nums1[j] && nums1[j-1] > 0 && 0 != j) {
-                    nums1[j] = nums2[i];
-                    break;
-                }
+        if(0 == n) {
+            return;
+        }
+        vector<int> result(m+n);
+        int p1 = 0, p2 = 0;
+        for(int i = 0; i < m + n; i++) {
+            if(p1 > m - 1) {
+                result[i] = nums2[p2++];
+                continue;
+            }
+            if(p2 > n -1) {
+                result[i] = nums1[p1++];
+                continue;
+            }
+            
+            if(nums1[p1] < nums2[p2]) {
+                result[i] = nums1[p1++];
+            } else {
+                result[i] = nums2[p2++];
             }
         }
+
+        nums1 = result;
+        // for(int i = 0; i < n; i++) {
+        //     cout << "i: " << i << " : " << nums2[i] << endl;
+        //     for(int j = 0; j < m + n; j++) {
+        //         cout << "i: " << i << " : " << nums2[i] << " j: " << j << " : " << nums1[j] << endl;
+        //         if(nums2[i] < nums1[j]) {
+        //             swap(nums2[i], nums1[j]);
+        //             i--;
+        //             break;
+        //         }
+
+        //         if(0 == nums1[j] && nums1[j-1] > 0 && 0 != j) {
+        //             nums1[j] = nums2[i];
+        //             break;
+        //         }
+        //     }
+        // }
     }
 };
-
-int main(int argc, char const *argv[])
-{
-    Solution cc;
-    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
-    vector<int> nums2 = {2, 5, 6};
-    cc.merge(nums1, 3, );
-    return 0;
-}
-[0,0,3,0,0,0,0,0,0]
-3
-[-1,1,1,1,2,3]
-6
