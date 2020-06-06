6ca7b8a334ef9f4ab4a0b74f2a4120f17797024e
hello_chenchen <welcomechenchen@gmail.com>
2020-06-06 22:25:32 +0800

    optimize leetcode 88 code

--- array/leetcode-88/solution.cc
+++ array/leetcode-88/solution.cc
@@ -7,44 +7,24 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(0 == n) {
-            return;
-        }
-        vector<int> result(m+n);
-        int p1 = 0, p2 = 0;
-        for(int i = 0; i < m + n; i++) {
-            if(p1 > m - 1) {
-                result[i] = nums2[p2++];
+        int totalNum = m + n;
+
+        int p1 = m - 1, p2 = n - 1;
+        for(int i = m + n - 1; i >= 0; i--) {
+            if(p1 < 0) {
+                nums1[i] = nums2[p2--];
                 continue;
             }
-            if(p2 > n -1) {
-                result[i] = nums1[p1++];
-                continue;
+
+            if(p2 < 0) {
+                return;
             }
-            
+
             if(nums1[p1] < nums2[p2]) {
-                result[i] = nums1[p1++];
+                nums1[i] = nums2[p2--];
             } else {
-                result[i] = nums2[p2++];
+                nums1[i] = nums1[p1--];
             }
         }
-
-        nums1 = result;
-        // for(int i = 0; i < n; i++) {
-        //     cout << "i: " << i << " : " << nums2[i] << endl;
-        //     for(int j = 0; j < m + n; j++) {
-        //         cout << "i: " << i << " : " << nums2[i] << " j: " << j << " : " << nums1[j] << endl;
-        //         if(nums2[i] < nums1[j]) {
-        //             swap(nums2[i], nums1[j]);
-        //             i--;
-        //             break;
-        //         }
-
-        //         if(0 == nums1[j] && nums1[j-1] > 0 && 0 != j) {
-        //             nums1[j] = nums2[i];
-        //             break;
-        //         }
-        //     }
-        // }
     }
 };
