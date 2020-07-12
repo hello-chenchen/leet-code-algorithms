a7d734bc918f357ff0fae34f9933a07a9e470377
hello_chenchen <welcomechenchen@gmail.com>
2020-07-12 13:34:14 +0800

    support leetcode

--- array/leetcode-485/solution.cc
+++ array/leetcode-485/solution.cc
@@ -1,14 +1,16 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
+        nums.push_back(0);  //FIXED: leetcode AddressSanitizer: heap-buffer-overflow on address 0x602000000140 at
         int result = 0;
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i < nums.size() - 1; i++) {
             int temp = 0;
-            while(nums[i] == 1) {
+            while(1 == nums[i]) {
                 temp++;
                 i++;
             }
@@ -23,5 +25,7 @@ public:
 int main(int argc, char const *argv[])
 {
     Solution cc;
+    vector<int> a = {1,1,0,1};
+    cout << cc.findMaxConsecutiveOnes(a);
     return 0;
 }
