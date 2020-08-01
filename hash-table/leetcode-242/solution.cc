ca2bf64838571aea07c26f074ab5d0643eeb7a27
hello_chenchen <welcomechenchen@gmail.com>
2020-08-01 18:12:57 +0800

    not support leetcode 242 on c++

--- hash-table/leetcode-242/solution.cc
+++ hash-table/leetcode-242/solution.cc
@@ -1,10 +1,41 @@
 #include <string>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        map<int, int> hashMap;
+        map<int, int>::iterator itr;
+        for(int i = 0; i < s.length(); i++) {
+            itr = hashMap.find(s[i]);
+            if(hashMap.end() != itr) {
+                itr->second++;
+            } else {
+                hashMap.insert(pair<int, int>(s[i], 1));
+            }
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            itr = hashMap.find(s[i]);
+            if(hashMap.end() != itr) {
+                itr->second--;
+            } else {
+                return false;
+            }
+        }
+
+        for (itr = hashMap.begin(); itr != hashMap.end(); ++itr) {
+            if(0 != itr->second) {
+                return false;
+            }
+        }
+
         return true;
     }
 };
