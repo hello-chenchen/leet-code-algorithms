f46c061b595afdc25523b7787fc6bfdec669ab0c
hello_chenchen <welcomechenchen@gmail.com>
2020-09-21 23:51:51 +0800

    use binary search optimize leetcode 69

--- binary-search/leetcode-69/solution.js
+++ binary-search/leetcode-69/solution.js
@@ -3,14 +3,28 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let result = 0;
-    for(let i = 0; i <= x; i++) {
-        if(i * i <= x) {
-            result = i;
+    return binarySearch(x, 0, x);
+};
+
+function binarySearch(x, start, end) {
+    let middle = parseInt((start + end) / 2);
+    if(middle > start && middle < end) {
+        if(middle * middle > x) {
+            return binarySearch(x, start, middle);
         } else {
-            break;
+            return binarySearch(x, middle, end);
+        }
+    } else {
+        if(middle * middle > x) {
+            return middle - 1;
+        } else if(middle * middle < x) {
+            if((middle + 1) * (middle + 1) > x) {
+                return middle;
+            } else {
+                return middle + 1;
+            }
+        } else {
+            return middle;
         }
     }
-
-    return result;
-};
+}
