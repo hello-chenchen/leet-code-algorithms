f7a5200c80d6169251094819bd61f2f27228c548
hello_chenchen <welcomechenchen@gmail.com>
2020-10-30 00:34:51 +0800

    support leetcode 976

--- sort/leetcode-976/solution.java
+++ sort/leetcode-976/solution.java
@@ -1,36 +1,38 @@
+import java.util.Arrays;
+
 class Solution {
     public int largestPerimeter(int[] A) {
         quickSort(A, 0, A.length - 1);
 
-    }
-
-    private void quickSort(int[] array, int start, int end) {
-        //array[end] is keyValue
-        int swapTemp = 0;
-        int left = start, right = end - 1;
-        while(left < right) {
-            if(array[left] > array[end] && array[right] < array[end]) {
-                swapTemp = array[left];
-                array[left] = array[right];
-                array[right] = swapTemp;
-                left++;
-                right--;
-            } else {
-                if(array[left] < array[end]) {
-                    left++;
-                } else {
-                    right--;
-                }
+        for(int i = A.length - 3; i >= 0; i--) {
+            if(A[i] + A[i+1] > A[i+2]) {
+                return A[i] + A[i+1] + A[i+2];
             }
         }
 
-        if(array[left] < array[end]) {
-            swapTemp = array[left];
-            array[left] = array[end];
-            array[end] = swapTemp;
+        return 0;
+    }
+
+    private void quickSort(int[] array, int start, int end) {
+        if(start >= end) return;
+
+        //array[end] is keyValue
+        int swapTemp = 0;
+        int left = start, right = end - 1;
+        for(int i = left; i <= right; i++) {
+            if(array[i] <= array[end]) {
+                swapTemp = array[left];
+                array[left] = array[i];
+                array[i] = swapTemp;
+                left++;
+            }
         }
 
+        swapTemp = array[left];
+        array[left] = array[end];
+        array[end] = swapTemp;
+
         quickSort(array, start, left - 1);
-        quickSort(array, right + 1, end);
+        quickSort(array, left + 1, end);
     }
 }
