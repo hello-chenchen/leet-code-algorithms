a184d4bccddcd4c762e5c01dbbabed773fcac365
hello_chenchen <welcomechenchen@gmail.com>
2020-11-14 23:07:45 +0800

    try to support leetcode 1356

--- sort/leetcode-1356/Solution.java
+++ sort/leetcode-1356/Solution.java
@@ -4,19 +4,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 class Solution {
-    private HashMap<Integer,ArrayList<Integer>> m_list = new HashMap<Integer,ArrayList<Integer>>();
+    private ArrayList<ArrayList<Integer>> m_arrayList = new ArrayList<ArrayList<Integer>>(14);
     public int[] sortByBits(int[] arr) {
+        int[] result = new int[arr.length];
         int temp = 0;
         for(int i = 0; i < arr.length; i++) {
             temp = containOneBinary(arr[i]);
-            if(true == m_list.containsKey(temp)) {
-                m_list.get(temp).add(arr[i]);
+            if(null == m_arrayList.get(temp)) {
+                m_arrayList.get(temp).add(arr[i]);
             } else {
-                m_list.put(temp, new ArrayList<Integer>(arr[i]));
+                m_arrayList.set(temp, new ArrayList<Integer>(arr[i]));
             }
         }
 
-        quickSort();
+        int index = 0;
+        for (ArrayList<Integer> item : m_arrayList) {
+            if(null != item) {
+                quickSort(item, 0, item.size() - 1);
+                for(Integer subItem : item) {
+                    result[index++] = subItem;
+                }
+            }
+       }
+
+       return result;
     }
 
     private int containOneBinary(int decNum) {
@@ -31,21 +42,21 @@ class Solution {
         return result;
     }
 
-    private void quickSort(int[] array, int start, int end) {
+    private void quickSort(ArrayList<Integer> array, int start, int end) {
         int left = start, right = end - 1;
         int swapTemp = 0;
         for(int i = left; i < right; i++) {
-            if(array[i] <= array[end]) {
-                swapTemp = array[left];
-                array[left] = array[i];
-                array[i] = swapTemp;
+            if(array.get(i) <= array.get(end)) {
+                swapTemp = array.get(left);
+                array.set(left, array.get(i));
+                array.set(i, swapTemp);
                 left++;
             }
         }
 
-        swapTemp = array[left];
-        array[left] = array[end];
-        array[end] = swapTemp;
+        swapTemp = array.get(left);
+        array.set(left, array.get(end));
+        array.set(end, swapTemp);
 
         quickSort(array, start, left - 1);
         quickSort(array, left + 1, end);
