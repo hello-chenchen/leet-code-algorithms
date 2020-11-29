efd27d607cf5dd361eb2b04d2cbeab50ed527202
hello_chenchen <welcomechenchen@gmail.com>
2020-11-29 14:50:00 +0800

    support leetcode 1356

--- sort/leetcode-1356/Solution.java
+++ sort/leetcode-1356/Solution.java
@@ -4,28 +4,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 class Solution {
-    private ArrayList<ArrayList<Integer>> m_arrayList = new ArrayList<ArrayList<Integer>>(14);
+    private ArrayList<Integer>[] m_arrayList = new ArrayList[14];
     public int[] sortByBits(int[] arr) {
         int[] result = new int[arr.length];
         int temp = 0;
         for(int i = 0; i < arr.length; i++) {
             temp = containOneBinary(arr[i]);
-            if(null == m_arrayList.get(temp)) {
-                m_arrayList.get(temp).add(arr[i]);
+            if(null != m_arrayList[temp]) {
+                m_arrayList[temp].add(arr[i]);
             } else {
-                m_arrayList.set(temp, new ArrayList<Integer>(arr[i]));
+                m_arrayList[temp] = new ArrayList<Integer>();
+                 m_arrayList[temp].add(arr[i]);
             }
         }
 
         int index = 0;
-        for (ArrayList<Integer> item : m_arrayList) {
-            if(null != item) {
-                quickSort(item, 0, item.size() - 1);
-                for(Integer subItem : item) {
+        for(int i = 0; i < m_arrayList.length; i++) {
+            if(null != m_arrayList[i]) {
+                quickSort(m_arrayList[i], 0, m_arrayList[i].size() - 1);
+                for(Integer subItem : m_arrayList[i]) {
                     result[index++] = subItem;
                 }
             }
-       }
+        }
 
        return result;
     }
@@ -33,19 +34,21 @@ class Solution {
     private int containOneBinary(int decNum) {
         int result = 0;
         while(0 != decNum) {
-            if(decNum != (decNum & 0xffff)) {
+            if(1 == (decNum & 0x0001)) {
                 result++;
-                decNum = decNum >> 1;
             }
+            decNum = decNum >> 1;
         }
 
         return result;
     }
 
     private void quickSort(ArrayList<Integer> array, int start, int end) {
+        if(start >= end) return;
+
         int left = start, right = end - 1;
         int swapTemp = 0;
-        for(int i = left; i < right; i++) {
+        for(int i = left; i <= right; i++) {
             if(array.get(i) <= array.get(end)) {
                 swapTemp = array.get(left);
                 array.set(left, array.get(i));
